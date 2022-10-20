d3f721fa14ac9a9662b56f07d5fce8f547b4c989
Matheus Mouser <matheusmouser2003@gmail.com>
2022-10-20 12:24:53 -0300

    inserção do projeto
    inserção do projeto

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,59 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt=""/>
+        </h1>
+        <button class="button" @click="alterarTema">
+           {{ textoDoBotao }}
+        </button>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BarraLateral',
+    emits: ['aoTemaAlterado'],
+    data () {
+        return {
+            modoEscuroAtivo: false
+        }
+    },
+    computed: {
+        textoDoBotao () {
+            if(this.modoEscuroAtivo) {
+                return 'Desativar DarkMode'
+            } 
+            return 'Ativar DarkMode'
+        }
+    },
+    methods: {
+        alterarTema() {
+            this.modoEscuroAtivo = !this.modoEscuroAtivo
+            this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
+        }
+    }
+})
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background: var(--bg-barra);
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+@media only screen and (max-width: 768px){
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+.button {
+    background-color: var(--bg-primario);
+    color: var(--texto-primario);
+}
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box has-text-weight-bold">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'BoxForm'
+})
+</script>
+
+<style scoped>
+.box{
+    background: var(--bg-card);
+    color: var(--texto-primario);
+}
+</style>
